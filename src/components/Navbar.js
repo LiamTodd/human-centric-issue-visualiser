376759b0750682aeeaa4e6f1ea4ff25881614ea5
LiamTodd <toddliam26@gmail.com>
2022-06-27 21:21:56 +1000

    neaten navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,23 +3,16 @@ import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <div className="navbar">
-      <ul>
-        <li>
-          <NavLink exact to="/">
-            Overview
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/list">List</NavLink>
-        </li>
-        <li>
-          <NavLink to="/prioritise">Prioritise</NavLink>
-        </li>
-        <li>
-          <NavLink to="/progress">Progress</NavLink>
-        </li>
-      </ul>
+    <div
+      className="navbar"
+      style={{ width: '100%', fontSize: '24px', padding: '20px' }}
+    >
+      <NavLink exact to="/">
+        Overview
+      </NavLink>
+      <NavLink to="/list">List</NavLink>
+      <NavLink to="/prioritise">Prioritise</NavLink>
+      <NavLink to="/progress">Progress</NavLink>
     </div>
   );
 }
