cbfedb94bdbb922554443ac8651d4ecf70b427ba
LiamTodd <toddliam26@gmail.com>
2022-07-01 15:33:21 +1000

    change probability of random HCI tag assignment

--- src/helpers/mockMLTool.js
+++ src/helpers/mockMLTool.js
@@ -3,7 +3,7 @@ export const predict = (body) => {
   const numberOfCategories = 3;
   let categories = [];
   for (let i = 0; i < numberOfCategories; i++) {
-    const rand = Math.floor(Math.random() * 2);
+    const rand = Math.floor(Math.random() * 8); // 1/8 chance to be assigned each HCI tag
     categories[i] = rand;
   }
   let flag = false;
