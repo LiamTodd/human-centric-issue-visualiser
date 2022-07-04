68a73ebc90f2f226f603af4d3d6e2791a03479ea
LiamTodd <toddliam26@gmail.com>
2022-07-04 23:53:03 +1000

    initialise doughnut chart

--- src/components/OverviewComponent.js
+++ src/components/OverviewComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import DoughnutChartComponent from './chartComponents/DoughnutChartComponent';
 import LineChartComponent from './chartComponents/LineChartComponent';
 import PolarAreaChartComponent from './chartComponents/PolarAreaChartComponent';
 import StackedBarChartComponent from './chartComponents/StackedBarChartComponent';
@@ -7,6 +8,7 @@ export default function OverviewComponent() {
   return (
     <>
       <PolarAreaChartComponent></PolarAreaChartComponent>
+      <DoughnutChartComponent></DoughnutChartComponent>
       <StackedBarChartComponent></StackedBarChartComponent>
       <LineChartComponent></LineChartComponent>
     </>
