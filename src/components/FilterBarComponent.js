51aee323505d319227cd567191b45e65384db1de
LiamTodd <toddliam26@gmail.com>
2022-07-11 00:38:33 +1000

    fix bug with unassigned status filtering

--- src/components/FilterBarComponent.js
+++ src/components/FilterBarComponent.js
@@ -8,14 +8,42 @@ const statusType = 'status';
 
 export default function FilterBarComponent({ setIssues }) {
   const options = [
-    { name: repoLabels.noHCIIdentifiedLabel.name, type: HCIType },
-    { name: repoLabels.appUsageLabel.name, type: HCIType },
-    { name: repoLabels.inclusivenessLabel.name, type: HCIType },
-    { name: repoLabels.userReactionLabel.name, type: HCIType },
-    { name: 'Unassigned Status', type: statusType },
-    { name: repoLabels.unresolvedHCILabel.name, type: statusType },
-    { name: repoLabels.resolvingHCILabel.name, type: statusType },
-    { name: repoLabels.resolvedHCILabel.name, type: statusType }
+    {
+      name: repoLabels.noHCIIdentifiedLabel.name,
+      type: HCIType,
+      value: repoLabels.noHCIIdentifiedLabel.name
+    },
+    {
+      name: repoLabels.appUsageLabel.name,
+      type: HCIType,
+      value: repoLabels.appUsageLabel.name
+    },
+    {
+      name: repoLabels.inclusivenessLabel.name,
+      type: HCIType,
+      value: repoLabels.inclusivenessLabel.name
+    },
+    {
+      name: repoLabels.userReactionLabel.name,
+      type: HCIType,
+      value: repoLabels.userReactionLabel.name
+    },
+    { name: 'Unassigned Status', type: statusType, value: null },
+    {
+      name: repoLabels.unresolvedHCILabel.name,
+      type: statusType,
+      value: repoLabels.unresolvedHCILabel.name
+    },
+    {
+      name: repoLabels.resolvingHCILabel.name,
+      type: statusType,
+      value: repoLabels.resolvingHCILabel.name
+    },
+    {
+      name: repoLabels.resolvedHCILabel.name,
+      type: statusType,
+      value: repoLabels.resolvedHCILabel.name
+    }
   ];
 
   const updateSelected = (selectedList, selectedOption) => {
@@ -28,11 +56,11 @@ export default function FilterBarComponent({ setIssues }) {
           // iterate over each selected option, return true if issue meets the criteria
           for (let i = 0; i < selectedList.length; i++) {
             if (selectedList[i].type == HCIType) {
-              if (issue.HCILabels.includes(selectedList[i].name)) {
+              if (issue.HCILabels.includes(selectedList[i].value)) {
                 return true;
               }
             } else if (selectedList[i].type == statusType) {
-              if (issue.progressTag == selectedList[i].name) {
+              if (issue.progressTag == selectedList[i].value) {
                 return true;
               }
             }
@@ -53,7 +81,7 @@ export default function FilterBarComponent({ setIssues }) {
         onRemove={updateSelected} // Function will trigger on remove event
         displayValue="name" // Property name to display in the dropdown options
         showCheckbox={true}
-        placeholder="Filter"
+        placeholder="Filter issues by..."
         avoidHighlightFirstOption={true}
         showArrow={true}
       ></Multiselect>
