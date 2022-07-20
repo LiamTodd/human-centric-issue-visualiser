c95fdec206e1f869323b93d24400a3a23ae7d274
LiamTodd <toddliam26@gmail.com>
2022-07-20 17:48:52 +1000

    set up display of complement tags

--- src/components/CorrectionIssueComponent.js
+++ src/components/CorrectionIssueComponent.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import * as uuid from 'uuid';
+import * as repoLabels from '../helpers/labels';
 
 import Card from 'react-bootstrap/Card';
 
 export default function CorrectionIssueComponent({ issue }) {
+  const allHCILabels = [
+    repoLabels.appUsageLabel,
+    repoLabels.inclusivenessLabel,
+    repoLabels.userReactionLabel
+  ];
   return (
     <>
       <Card border="secondary">
@@ -18,22 +24,50 @@ export default function CorrectionIssueComponent({ issue }) {
           {issue.body}
           Raised by {issue.user.login}, {issue.created_at.slice(0, 10)}
           <br></br>
-          HCIs:
+          <br></br>
+          Click to unassign from this issue:
           <div style={{ justifyContent: 'center', display: 'flex' }}>
             {issue.HCILabels.map((HCILabel) => {
               return (
-                <div
+                <button
                   style={{
                     margin: '10px',
                     width: '20%',
                     borderRadius: '20px',
+                    borderWidth: '0px',
                     backgroundColor: '#' + HCILabel.color + '80' // change opacity
                   }}
                   key={uuid.v4()}
                 >
                   {HCILabel.name}
-                </div>
+                </button>
               );
+            })}
+          </div>
+          <br></br>
+          Click to assign to this issue:
+          <div style={{ justifyContent: 'center', display: 'flex' }}>
+            {allHCILabels.map((HCILabel) => {
+              if (
+                !issue.HCILabels.map((label) => {
+                  return label.name;
+                }).includes(HCILabel.name)
+              ) {
+                return (
+                  <button
+                    style={{
+                      margin: '10px',
+                      width: '20%',
+                      borderRadius: '20px',
+                      borderWidth: '0px',
+                      backgroundColor: '#' + HCILabel.color + '40' // change opacity
+                    }}
+                    key={uuid.v4()}
+                  >
+                    {HCILabel.name}
+                  </button>
+                );
+              }
             })}
           </div>
           <Card.Text>ML Confidence: {issue.confidence}</Card.Text>
