244e13dd3ec6790a876d7a6e551f37a464ccf6e8
LiamTodd <toddliam26@gmail.com>
2022-07-20 16:14:16 +1000

    add descriptions to overview

--- src/components/OverviewComponent.js
+++ src/components/OverviewComponent.js
@@ -7,6 +7,26 @@ import StackedBarChartComponent from './chartComponents/StackedBarChartComponent
 import UnAuthenticatedDefault from './UnAuthenticatedDefault';
 
 export default function OverviewComponent() {
+  const polarChartDescription =
+    'This chart provides a visualisation of the relative amounts of different Human Centric Issues identified in the GitHub repository. The colours correspond to the different Human Centric Issue categories, while the radius corresponds to the number of times a type of Human Centric Issue has been identified within the repository. Note that each issue in the repository may be assigned more than one Human Centric Issue tag.';
+  const polarChartUseCase =
+    'This chart can be used to identify what types of Human Centric Issues are/are not appearing frequently in the repository, and thus can help lead discussions regarding how to focus development efforts towards creating an application which is conscious of Human Centric Issues.';
+
+  const horizontalChartDescription =
+    'This chart provides an overview of the progress status of each issue in the repository. Each bar corresponds to one of the four possible statuses (including "unassigned"), and the length of each bar corresponds to the number of issues in the GitHub repository with said progress status.';
+  const horizontalChartUseCase =
+    'This chart can be used to monitor whether the issues in the repository are being attended to or not, and can help ensure that there is not a pile-up of issues which are not being attended to.';
+
+  const stackedBarChartDescription =
+    'This chart provides a comprehensive summary of how the different Human Centric Issue categories have been identified in the GitHub repository over time. The horizontal axis corresponds to the dates at which issues have been raised in the GitHub repository, while the colours and length of the bars show how many of each Human Centric Issue tag has been applied to said issue(s) on each day. Colour is used to distinguish between the various Human Centric Issue categorisations.';
+  const stackedBarChartUseCase =
+    'This chart can be used to analyse how, at different stages of the software lifecycle, different types of Human Centric Issues become more and less relevant. For example, during the maintentance phase, there may be an increase in "User Reaction" Human Centric Issues. This information can help guide the focus of the team(s), behind the software throughout the software lifecycle.';
+
+  const verticalBarChartDescription =
+    'This chart provides a simple overview of the prioritisation levels of all issues in the GitHub repository. Each bar corresponds to one of the four possible priority levels (including "unassigned"), and the height of each bar corresponds to the number of issues in the GitHub repository with said prioritisation level.';
+  const verticalBarChartUseCase =
+    "This chart can be used to easily gauge the level of urgency with which the repository's issues need to be attended to.";
+
   return (
     <>
       {!JSON.parse(localStorage.getItem(READY_KEY)) && (
@@ -36,12 +56,13 @@ export default function OverviewComponent() {
             <div
               style={{
                 paddingTop: '5vh',
-                width: '25%'
+                width: '30%',
+                paddingRight: '5%'
               }}
             >
-              test test test testtest testtest testtest testtest testtest
-              testtest testtest test test testtest testtest testtest testtest
-              testtest testtest testtest testtest testtest testtest test
+              <div style={{ fontSize: 'larger' }}>{polarChartDescription}</div>
+              <p></p>
+              {polarChartUseCase}
             </div>
           </div>
 
@@ -58,21 +79,25 @@ export default function OverviewComponent() {
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
-                width: '80%'
+                width: '70%'
               }}
             >
               <HorizontalBarChartComponent></HorizontalBarChartComponent>
             </div>
-            <div style={{ width: '15%', paddingTop: '5vh' }}>
-              test test test testtest testtest testtest testtest testtest
-              testtest testtest test test testtest testtest testtest testtest
-              testtest testtest testtest testtest testtest testtest test
+            <div
+              style={{ width: '30%', paddingTop: '5vh', paddingRight: '5%' }}
+            >
+              <div style={{ fontSize: 'larger' }}>
+                {horizontalChartDescription}
+              </div>
+              <p></p>
+              {horizontalChartUseCase}
             </div>
           </div>
 
           <div
             style={{
-              display: 'flex',
+              // display: 'flex',
               borderBottom: '2px solid lightgrey',
               paddingBottom: '25px',
               paddingTop: '25px'
@@ -83,15 +108,23 @@ export default function OverviewComponent() {
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
-                width: '75%'
+                width: '90%'
               }}
             >
               <StackedBarChartComponent></StackedBarChartComponent>
             </div>
-            <div style={{ width: '20%', paddingTop: '5vh', paddingLeft: '5%' }}>
-              test test test testtest testtest testtest testtest testtest
-              testtest testtest test test testtest testtest testtest testtest
-              testtest testtest testtest testtest testtest testtest test
+            <div
+              style={{
+                paddingTop: '5vh',
+                paddingLeft: '5%',
+                paddingRight: '5%'
+              }}
+            >
+              <div style={{ fontSize: 'larger' }}>
+                {stackedBarChartDescription}
+              </div>
+              <p></p>
+              {stackedBarChartUseCase}
             </div>
           </div>
 
@@ -113,10 +146,14 @@ export default function OverviewComponent() {
             >
               <PriorityBarChartComponent></PriorityBarChartComponent>
             </div>
-            <div style={{ width: '15%', paddingTop: '5vh' }}>
-              test test test testtest testtest testtest testtest testtest
-              testtest testtest test test testtest testtest testtest testtest
-              testtest testtest testtest testtest testtest testtest test
+            <div
+              style={{ width: '30%', paddingTop: '5vh', paddingRight: '5%' }}
+            >
+              <div style={{ fontSize: 'larger' }}>
+                {verticalBarChartDescription}
+              </div>
+              <p></p>
+              {verticalBarChartUseCase}
             </div>
           </div>
         </div>
