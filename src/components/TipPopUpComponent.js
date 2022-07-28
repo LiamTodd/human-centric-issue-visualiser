6b8250149760d7bf56355a100c24980b4c6749d4
LiamTodd <toddliam26@gmail.com>
2022-07-28 10:45:03 +1000

    add extended descriptions to popup

--- src/components/TipPopUpComponent.js
+++ src/components/TipPopUpComponent.js
@@ -15,29 +15,48 @@ export default function TipPopUpComponent() {
     inclusivenessLabel,
     noHCIIdentifiedLabel
   ];
+  const extendedDescriptions = [
+    appUsageLabel.description +
+      'This category consists of Resource Usage, Buginess, Change & Update, UI & UX, Privacy & Security, Usage Instruction, Access Issues, and Others.',
+    userReactionLabel.description +
+      'We further divide this into Fulfilling interests, Emotional aspects, Preference, and Others.',
+    inclusivenessLabel.description +
+      'It includes issues related to the age, gender, and socioeconomic status of the users. We categorise Inclusiveness into five different subcategories, as: Compatibility, Location, Language, Accessibility, and Others.',
+    ''
+  ];
   const popover = (
-    <Popover id="popover-basic">
+    <Popover id="popover-basic" style={{ maxWidth: '70vw' }}>
       <Popover.Header as="h3" style={{ backgroundColor: '#9AC7BF' }}>
         Human Centric Issue Categories
       </Popover.Header>
       <Popover.Body>
         <div style={{}}>
-          {allHCILabels.map((label) => {
+          {allHCILabels.map((label, i) => {
             return (
-              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <div>
                 <div
                   style={{
-                    borderRadius: '100%',
-                    height: '4vh',
-                    width: '4vh',
-                    borderColor: 'grey',
-                    padding: '10px',
-                    margin: '0.2%',
-                    backgroundColor: '#' + label.color + '80'
+                    display: 'flex',
+                    justifyContent: 'left'
                   }}
-                ></div>
-                <div style={{ fontSize: '16px' }}>{label.name}</div>
+                >
+                  <div
+                    style={{
+                      borderRadius: '100%',
+                      height: '4vh',
+                      width: '4vh',
+                      borderColor: 'grey',
+                      padding: '10px',
+                      margin: '0.2%',
+                      backgroundColor: '#' + label.color + '80'
+                    }}
+                  ></div>
+                  <div style={{ fontSize: '20px', paddingLeft: '20px' }}>
+                    {label.name}
+                  </div>
+                </div>
                 <br></br>
+                <p style={{ fontSize: '16px' }}>{extendedDescriptions[i]}</p>
               </div>
             );
           })}
