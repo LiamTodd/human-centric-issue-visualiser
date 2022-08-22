96ecb3e415199354af541cf42f1adc16f5e5468f
LiamTodd <toddliam26@gmail.com>
2022-08-22 14:32:30 +1000

    prefix labels to mitigate clashes on random repos

--- src/helpers/labels.js
+++ src/helpers/labels.js
@@ -1,61 +1,62 @@
+const prefix = 'HCI: ';
 export const noHCIIdentifiedLabel = {
-  name: 'Non Human-Centric',
+  name: prefix + 'Non Human-Centric',
   description:
     'This issue does not contain human-centric discussion - Human Centric Issues dashboard.',
   color: '9AC7BF'
 };
 
 export const userReactionLabel = {
-  name: 'User Reaction',
+  name: prefix + 'User Reaction',
   description:
     'This issue contains discussion around user reaction - Human Centric Issues dashboard.',
   color: 'FF66FF'
 };
 export const inclusivenessLabel = {
-  name: 'Inclusiveness',
+  name: prefix + 'Inclusiveness',
   description:
     'This issue contains discussion around inclusiveness - Human Centric Issues dashboard.',
   color: '6666FF'
 };
 export const appUsageLabel = {
-  name: 'App Usage',
+  name: prefix + 'App Usage',
   description:
     'This issue contains discussion around app usage- Human Centric Issues dashboard.',
   color: '66B2FF'
 };
 
 export const unresolvedHCILabel = {
-  name: 'Unresolved',
+  name: prefix + 'Unresolved',
   description:
     'This issue is unresolved. This label was created by the Human Centric Issues dashboard.',
   color: 'C1867B'
 };
 export const resolvingHCILabel = {
-  name: 'Resolving',
+  name: prefix + 'Resolving',
   description:
     'This issue is currently being resolved - Human Centric Issues dashboard.',
   color: '9AB5C1'
 };
 export const resolvedHCILabel = {
-  name: 'Resolved',
+  name: prefix + 'Resolved',
   description: 'This issue has been resolved - Human Centric Issues dashboard.',
   color: 'EBEDC8'
 };
 
 export const lowPriorityLabel = {
-  name: 'Low Priority',
+  name: prefix + 'Low Priority',
   description:
     'This issue is a low priority item - Human Centric Issues dashboard.',
   color: '00CC66'
 };
 export const mediumPriorityLabel = {
-  name: 'Medium Priority',
+  name: prefix + 'Medium Priority',
   description:
     'This issue is a medium priority item - Human Centric Issues dashboard..',
   color: 'FF9933'
 };
 export const highPriorityLabel = {
-  name: 'High Priority',
+  name: prefix + 'High Priority',
   description:
     'This issue is a high priority item - Human Centric Issues dashboard.',
   color: 'F19C99'
